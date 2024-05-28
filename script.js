0113b10761e13d05852f9d029979dd61119273a6
ratnakargiri973 <163234593+ratnakargiri973@users.noreply.github.com>
2024-05-28 11:08:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
 
+let start = document.getElementById('info1');
+let pressed = document.getElementById('pressed');
+let value = document.getElementById('value');
+pressed.style.display = "none";
+value.style.display = "none";
+// result.style.backgroundColor = "none";
 window.addEventListener('keydown', function(event){
+        start.style.display="none";
         const key = event.key;
         const value = document.getElementById('value');
         const keyCode = event.keyCode || event.which;
@@ -10,4 +17,7 @@ window.addEventListener('keydown', function(event){
     let sound = document.getElementById('sound');
             sound.currentTime = 0;
             sound.play();
+            pressed.style.display = "block";
+            value.style.display = "block";  
+           
 })
